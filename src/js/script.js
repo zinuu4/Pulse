f7f58189d802a048e4623164da961edbf543bb07
Zinoviy <unvm7721@gmail.com>
2023-04-17 15:35:00 +0300

    added forms validation

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,37 @@ $(document).ready(function(){
       $('.overlay, #order').fadeIn('slow');
     })
   });
+
+  // Validation
+  
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!")
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
 });
